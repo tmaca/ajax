ff8c54664b550613623a3e92d41123294d9f7f80
tmaca <tmaca312@gmail.com>
2017-10-31 12:48:27 +0100

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+
+    </head>
+<script>
+
+    function llamarAjax() {
+        var http = new XMLHttpRequest();
+        var url = "respuesta?nombre=" + document.getElementById("formulario").elements["nombre"].value;
+        http.open("GET", url, true);
+        http.onreadystatechange = function () {
+            if (
+                http.readyState == 4) {
+// Se ha recibido la respuesta.
+                if (http.status == 200) {
+// Aquí escribiremos lo que queremos que
+// se ejecute tras recibir la respuesta
+                    var datosDoc = http.responseText;
+                    alert(datosDoc);
+                } else {
+// Ha ocurrido un error
+                    alert("Error:" + http.statusText);
+                }
+            }
+        };
+        http.send(null);
+
+    }
+
+</script>
+<body>
+
+<form id="formulario" >
+    <p>Nombre: <input name="nombre" type="text"/> </p>
+    <input onclick="llamarAjax()" value="click"/>
+</form>
+
+
+
+</body>
+</html>
